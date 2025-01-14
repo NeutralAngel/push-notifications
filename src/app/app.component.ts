7e1f562f308c0e887d409bbea412758000bcdb3b
opr7552 <nathan.nontell@infarmbureau.com>
2025-01-14 09:13:29 -0500

    subscribe button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'push_notifications_sample';
+  readonly VAPID_PUBLIC_KEY =
+    'BPE45LPjX4r2n9-Gd4aL5OhghbtInabVNJnVr1RiUtwAltXIgAZqkB0tEgaiPuLEg-qQfg7NwVvxHfyhKK2ifWQ';
+
+  constructor(private swPush: SwPush) {}
+
+  subscribe() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then((sub) => console.log(JSON.stringify(sub)));
+  }
 }
